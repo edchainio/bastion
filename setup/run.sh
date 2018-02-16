f64ed02fbfc6d011e988f9a926425cfde5091082
trabing <kenso.trabing@bloomwebsite.com>
2018-02-16 03:58:05 -0500

    Start to limit journal expansion

--- setup/run.sh
+++ setup/run.sh
@@ -2,6 +2,21 @@
 
 # Testing
 
+echo "
+
+
+___________________________________ TESTING ___________________________________
+
+This is program is currently in testing. 
+
+You can change this setting in run.sh.
+
+
+
+"
+
+sleep 3
+
 rm logs/*.json procedures/*.sh
 
 cp -r originals/*.sh procedures
